6af35073c34e343b3069ce9b642c94a410464148
Andrew Christie <Andrew.Christie@ipreo.com>
2016-09-26 16:49:59 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import '@ipreo/cupcake-styles/dist/default/default.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
